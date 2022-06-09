d41c71e2108a37ec55925163f2d6d83453676add
SkreimOF <2001main2001@gmail.com>
2022-06-09 20:58:31 +0200

    Добавьте файлы проекта.

---
+++ 13.6.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FavoriteWords
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            string text = File.ReadAllText("D:\\Downloads\\test1.txt");
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            Dictionary<string, int> wordsCounter = new Dictionary<string, int>();
+            foreach (string s in wordsCounter.Keys)
+            {
+                if (wordsCounter.ContainsKey(s)) wordsCounter[s]++;
+                else wordsCounter.Add(s, 1);
+            }
+
+
+        }
+    }
+}
+
+
+
+
+
